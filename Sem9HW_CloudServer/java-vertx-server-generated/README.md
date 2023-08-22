49ec3aa38813182fe5ab954b38ee8151f26ec021
julemalysheva <julemalysheva@gmail.com>
2023-08-23 02:09:55 +0500

    cloud erd UI|UX openapi

---
+++ Sem9HW_CloudServer/java-vertx-server-generated/README.md
@@ -0,0 +1,1 @@
+Project generated on : 2023-08-22T21:03:34.031334430Z[GMT]
---
+++ Sem9HW_CloudServer/java-vertx-server-generated/src/main/java/io/swagger/server/api/service/CloudsApi.java
@@ -0,0 +1,29 @@
+package io.swagger.server.api.service;
+
+import io.swagger.server.api.model.Clouds;
+import io.swagger.server.api.model.Error;
+
+import io.vertx.core.Handler;
+import io.vertx.core.AsyncResult;
+import io.vertx.ext.web.validation.RequestParameter;
+import io.vertx.ext.web.api.service.ServiceRequest;
+import io.vertx.ext.web.api.service.ServiceResponse;
+import io.vertx.ext.web.api.service.WebApiServiceGen;
+
+import java.util.List;
+import java.util.Map;
+
+@WebApiServiceGen
+public interface CloudsApi  {
+
+    String WEBSERVICE_ADDRESS_CLOUDSAPI = "";
+    String OPERATION_ID_CANCELCLOUDBYID = "cancelCloudById";
+    String OPERATION_ID_CREATECLOUD = "createCloud";
+    String OPERATION_ID_GETALLCLOUDS = "getAllClouds";
+    String OPERATION_ID_GETCLOUDBYID = "getCloudById";
+
+    void cancelCloudById(RequestParameter body, ServiceRequest request, Handler<AsyncResult<ServiceResponse>> resultHandler);
+    void CreateCloud(RequestParameter body, ServiceRequest request, Handler<AsyncResult<ServiceResponse>> resultHandler);
+    void getAllClouds(RequestParameter body, ServiceRequest request, Handler<AsyncResult<ServiceResponse>> resultHandler);
+    void getCloudById(RequestParameter body, ServiceRequest request, Handler<AsyncResult<ServiceResponse>> resultHandler);
+}
